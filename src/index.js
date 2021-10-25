6b0727671f9d9ee80dacc9e4378b2a41d71e5987
Stef Käser <info.stef.kaeser@gmail.com>
2021-10-25 23:37:09 +0200

    hiking time estimate

--- src/index.js
+++ src/index.js
@@ -48,6 +48,14 @@ function addToMap(gpxFileName) {
       e.target.bindTooltip(getHtmlInfoElement(e.target._info));
       map.fitBounds(e.target.getBounds());
     })
+    .on("addline", function (l) {
+      let totalHikingTime = calculateHikingTime(l.line.getLatLngs());
+      var minutes = parseInt(
+        (totalHikingTime - parseInt(totalHikingTime)) * 60
+      );
+      l.sourceTarget._info.estimatedHikingTimeInHours =
+        parseInt(totalHikingTime) + "h " + minutes + "min";
+    })
     .addTo(map);
 }
 
@@ -56,9 +64,11 @@ function getHtmlInfoElement(tourInfo) {
     <h1>${tourInfo.name}</h1>
     <div>Up: ${Math.round(tourInfo.elevation.gain)} m</div>
     <div>Down: ${Math.round(tourInfo.elevation.loss)} m</div>
-    <div>Max: ${Math.round(tourInfo.elevation.max)} m</div>
-    <div>Min: ${Math.round(tourInfo.elevation.min)} m</div>
-    <div>Length: ${Math.round(tourInfo.length / 10) / 100} km</div> 
+    <div>Max elevation: ${Math.round(tourInfo.elevation.max)} m</div>
+    <div>Min elevation: ${Math.round(tourInfo.elevation.min)} m</div>
+    <div>Length: ${Math.round(tourInfo.length / 10) / 100} km</div>
+    <div>Estimate: ${tourInfo.estimatedHikingTimeInHours}</div>
+
     </div>`;
   return template;
 }
@@ -102,4 +112,72 @@ function addLayer(layer, layerName) {
   });
   map.addLayer(layer);
   currentMapTileLayer = layer._leaflet_id;
+}
+
+//https://www.wanderndeluxe.de/en/calculate-hiking-time-distance-altitude/
+function calculateHikingTime(l) {
+  var lastPoint = null;
+  var totalHikingTimeDistance = 0;
+  var totalElevation = 0;
+  var totalDistance = 0;
+  var totalHikingTimeElevation = 0;
+  l.forEach((x) => {
+    if (lastPoint == null) {
+      lastPoint = x;
+      return;
+    }
+
+    var distanceInKm = getDistanceFromLatLonInKm(
+      x.lat,
+      x.lng,
+      lastPoint.lat,
+      lastPoint.lng
+    );
+
+    if (isNaN(lastPoint.meta.ele) || isNaN(x.meta.ele)) {
+      totalHikingTimeElevation = totalHikingTimeDistance = NaN;
+      return;
+    }
+
+    var elevation = x.meta.ele - lastPoint.meta.ele;
+    totalElevation += elevation;
+    totalDistance += distanceInKm;
+    totalHikingTimeElevation += getHikingTimeElevation(elevation);
+    totalHikingTimeDistance += distanceInKm / 4; // 4KM / h
+    lastPoint = x;
+  });
+
+  var time =
+    Math.max(totalHikingTimeElevation, totalHikingTimeDistance) +
+    Math.min(totalHikingTimeElevation, totalHikingTimeDistance) / 2;
+  return time;
+}
+
+function getHikingTimeElevation(elevationInMeter) {
+  //ascent
+  var elevationFactor = elevationInMeter / 300; //300hm / h
+  if (elevationInMeter < 0) {
+    //descent
+    elevationFactor = (elevationInMeter * -1) / 500; //500hm / h
+  }
+  return elevationFactor;
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
