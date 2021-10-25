5161b73b1ae6fa91c9b63a7b773cc749de2b315d
Stef Käser <info.stef.kaeser@gmail.com>
2021-10-25 21:33:24 +0200

    add swisstopo as tilelayer

--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
-(function (window) {
-  "use strict";
-  var map = null;
-  function initMap() {
-    var L = window.L;
-    var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "Map data &copy; 2021 OpenStreetMap contributors",
-    });
-    map = L.map("map", {
-      center: [0, 0],
-      zoom: 2,
-    }).addLayer(osm);
-  }
+var map = null;
+var currentMapTileLayer = null;
 
-  function initRoutes() {
-    var jsonUrl =
-      location.href.indexOf("127.0.0.1") >= 0
-        ? "routes/routes_generated_sample.json"
-        : "routes/routes_generated.json";
+function initMap() {
+  var L = window.L;
 
-    try {
-      fetch(jsonUrl)
-        .then((response) => response.json())
-        .then((result) => {
-          console.log(result);
-          result.routes.forEach((route) => {
-            console.log(route.Name);
-            addToMap("routes/" + route.Name);
-          });
+  map = L.map("map", {
+    center: [0, 0],
+    zoom: 2,
+  });
+  setMapLayer();
+}
+
+function initRoutes() {
+  var jsonUrl =
+    location.href.indexOf("127.0.0.1") >= 0
+      ? "routes/routes_generated_sample.json"
+      : "routes/routes_generated.json";
+
+  try {
+    fetch(jsonUrl)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        result.routes.forEach((route) => {
+          console.log(route.Name);
+          addToMap("routes/" + route.Name);
         });
-    } catch (e) {
-      console.log(e);
-    }
+      });
+  } catch (e) {
+    console.log(e);
   }
+}
 
-  function addToMap(gpxFileName) {
-    new L.GPX(gpxFileName, {
-      async: true,
-      marker_options: {
-        startIconUrl: "images/pin-icon-start.png",
-        endIconUrl: "images/pin-icon-end.png",
-        shadowUrl: "images/pin-shadow.png",
-        wptIconUrls: {
-          "": null,
-        },
+function addToMap(gpxFileName) {
+  new L.GPX(gpxFileName, {
+    async: true,
+    marker_options: {
+      startIconUrl: "images/pin-icon-start.png",
+      endIconUrl: "images/pin-icon-end.png",
+      shadowUrl: "images/pin-shadow.png",
+      wptIconUrls: {
+        "": null,
       },
+    },
+  })
+    .on("loaded", function (e) {
+      e.target.bindTooltip(getHtmlInfoElement(e.target._info));
+      map.fitBounds(e.target.getBounds());
     })
-      .on("loaded", function (e) {
-        e.target.bindTooltip(getHtmlInfoElement(e.target._info));
-        map.fitBounds(e.target.getBounds());
-      })
-      .addTo(map);
-  }
+    .addTo(map);
+}
 
-  function getHtmlInfoElement(tourInfo) {
-    var template = `<div class="tourpopup">
+function getHtmlInfoElement(tourInfo) {
+  var template = `<div class="tourpopup">
     <h1>${tourInfo.name}</h1>
     <div>Up: ${Math.round(tourInfo.elevation.gain)} m</div>
     <div>Down: ${Math.round(tourInfo.elevation.loss)} m</div>
@@ -61,11 +60,46 @@
     <div>Min: ${Math.round(tourInfo.elevation.min)} m</div>
     <div>Length: ${Math.round(tourInfo.length / 10) / 100} km</div> 
     </div>`;
-    return template;
-  }
+  return template;
+}
 
-  window.addEventListener("load", function () {
-    initMap();
-    initRoutes();
+window.addEventListener("load", function () {
+  initMap();
+  initRoutes();
+});
+
+function setMapLayer() {
+  if (localStorage.getItem("mapLayer") == "OpenStreetMap") {
+    changeToOpenStreetMap();
+  } else {
+    changeToSwissTopo();
+  }
+}
+
+function changeToOpenStreetMap() {
+  var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Map data &copy; 2021 OpenStreetMap contributors",
   });
-})(window);
+  addLayer(osm, "OpenStreetMap");
+}
+
+function changeToSwissTopo() {
+  var swissTopo = L.tileLayer(
+    "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
+    {
+      attribution: "swisstopo",
+    }
+  );
+  addLayer(swissTopo, "SwissTopo");
+}
+
+function addLayer(layer, layerName) {
+  localStorage.setItem("mapLayer", layerName);
+  map.eachLayer((x) => {
+    if (x._leaflet_id == currentMapTileLayer) {
+      map.removeLayer(x);
+    }
+  });
+  map.addLayer(layer);
+  currentMapTileLayer = layer._leaflet_id;
+}
--- src/process/createRoutesJson.ps1
+++ src/process/createRoutesJson.ps1
@@ -2,14 +2,14 @@ $routes = (dir  "./../routes")
 
 $allRoutes = $routes | Select-Object -Property Name
 $jsonAll = @(
-       [pscustomobject]@{routes=$allRoutes}
- ) | ConvertTo-Json
+      [pscustomobject]@{routes = $allRoutes }
+) | ConvertTo-Json
     
 $jsonAll | Out-File -Encoding UTF8 "./../routes/routes_generated.json"
 
-$sampleRoutes = $routes | Select-Object -Property Name -First 5
+$sampleRoutes = $routes | Select-Object -Property Name -First 2
 $jsonSample = @(
-       [pscustomobject]@{routes=$sampleRoutes}
- ) | ConvertTo-Json
+      [pscustomobject]@{routes = $sampleRoutes }
+) | ConvertTo-Json
     
 $jsonSample | Out-File -Encoding UTF8 "./../routes/routes_generated_sample.json"
