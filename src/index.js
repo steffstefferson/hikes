6616f9a1cc6783a83526298f7139a4986c386b11
Stef Käser <info.stef.kaeser@gmail.com>
2022-07-16 21:39:18 +0200

    set scope for sw

--- src/index.js
+++ src/index.js
@@ -245,10 +245,11 @@ export function changeToSwissTopo() {
   addLayer(swissTopo, "SwissTopo");
 }
 
+var swScope = location.href.indexOf("localhost") >= 0 ? "." : "/hikes/src/";
 export function installServiceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("service-worker.js", { scope: "." })
+      .register("service-worker.js", { scope: swScope })
       .then((reg) => {
         console.log("Registration succeeded. Scope is " + reg.scope);
       })
