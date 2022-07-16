8779ec65e91ac37e8e246b9a26735455aa1ab42b
Stef Käser <info.stef.kaeser@gmail.com>
2022-07-16 20:50:25 +0200

    add default url

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,6 +8,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("v1").then((cache) => {
       return cache.addAll([
+        "/",
         "index.html",
         "custom_tracks.html",
         "styles.css",
